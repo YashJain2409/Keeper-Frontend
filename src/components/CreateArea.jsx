1e8529bd39ac708563696b2496f52409989167db
YashJain2409 <yashsj24@gmail.com>
2022-02-22 20:14:56 +0530

    addSpeechRecognitionToContent

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,16 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
 import Zoom from "@mui/material/Zoom";
+import MicIcon from "@mui/icons-material/Mic";
+import MicOffIcon from "@mui/icons-material/MicOff";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.lang = "en-US";
+recognition.interimResults = true;
 
 function CreateArea(props) {
-  const [valid,setValid] = useState(true);
+  const [valid, setValid] = useState(true);
   const [vis, setVis] = useState(false);
+  const [isListening, setIsListening] = useState(false);
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
 
+  useEffect(() => {
+    handleSpeech();
+  }, [isListening]);
+  function handleSpeech() {
+    let content = note.content;
+    if (isListening) {
+      recognition.start();
+      console.log("started");
+      recognition.onend = () => {
+        console.log("continue...");
+        recognition.start();
+      };
+    } else {
+      recognition.stop();
+      recognition.onend = () => {
+        console.log("stopped");
+      };
+      return;
+    }
+    recognition.onresult = (event) => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      setNote((prevNote) => {
+        return {
+          ...prevNote,
+          content: content + transcript + " ",
+        };
+      });
+    };
+  }
   function handleChange(event) {
     const { name, value } = event.target;
 
@@ -27,7 +69,7 @@ function CreateArea(props) {
       setValid(false);
       setTimeout(() => {
         setValid(true);
-      },1000);
+      }, 1000);
       return;
     }
     props.onAdd(note);
@@ -58,13 +100,40 @@ function CreateArea(props) {
           onClick={() => {
             setVis(true);
           }}
+          readOnly={isListening ? "readOnly" : ""}
         />
+        {vis &&
+          (isListening ? (
+            <MicIcon
+              style={{
+                cursor: "pointer",
+                color: "#f5ba13",
+              }}
+              onClick={() => {
+                setIsListening((prevState) => !prevState);
+              }}
+            />
+          ) : (
+            <MicOffIcon
+              style={{
+                cursor: "pointer",
+                color: "#f5ba13",
+              }}
+              onClick={() => {
+                setIsListening((prevState) => !prevState);
+              }}
+            />
+          ))}
         <Zoom in={vis}>
           <Fab onClick={submitNote}>
             <AddIcon />
           </Fab>
         </Zoom>
-        {!valid && <p style={{color: 'red'}}>please add some data , some fields are empty.</p>}
+        {!valid && (
+          <p style={{ color: "red" }}>
+            please add some data , some fields are empty.
+          </p>
+        )}
       </form>
     </div>
   );
